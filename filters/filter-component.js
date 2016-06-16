7889f84ce0fac699103e6d15b1419bc8ad57b3b7
ValYouW <ValYouW@gmail.com>
2016-06-16 09:48:14 +0300

    Raise event when clicking apply filters btn

--- filters/filter-component.js
+++ filters/filter-component.js
@@ -16,24 +16,35 @@
 	FilterController.$inject = ['$element', '$scope'];
 
 	FilterController.prototype.$onChanges = function(changes) {
-		// Check if the data has changed
+		// Check if the filter has changed
 		if (typeof changes.filter !== 'undefined' && this.filter) {
-			this.inputType = this.filter && this.filter.multi ? 'checkbox' : 'radio';
+			this.filter.multi = !!this.filter.multi;
+			this.inputType = this.filter.multi ? 'checkbox' : 'radio';
 
-			// Make sure we have valid options
+			// Make sure we have valid options array
 			if (!angular.isArray(this.filter.options)) {
 				this.filter.options = [];
 			}
 
-			// Add a "pathId" to the option which includes the filter id and option id
+			// Normalize the options
 			for (var i = 0; i < this.filter.options.length; ++i) {
 				var opt = this.filter.options[i];
+
+				// Add a "pathId" to the option which includes the filter id and option id (to create a unique id for the input element)
 				opt.pathId = this.filter.id + '_' + opt.id;
 				opt.selected = opt.selected || false;
 				opt.count = typeof opt.count === 'number' ? opt.count : -1;
+
+				// If this is "radio" we need to set the Filter's selected value to the specific selected option
+				if (!this.filter.multi && opt.selected) {
+					this.filter.selected = opt.id;
+				}
 			}
 
-			this.filter.selected = this.filter.selected || '';
+			// If this is "radio" and there is no item selected we need to selected the "_ALL_" radio
+			if (!this.filter.multi && !this.filter.selected) {
+				this.filter.selected = '_ALL_';
+			}
 		}
 	};
 
@@ -48,23 +59,40 @@
 
 	FilterController.prototype.selectionChanged = function(e) {
 		var selection = [];
+
+		// Checkboxes
 		if (this.filter.multi) {
+			// Check if this is the "ALL" checkbox
 			if (e.target && e.target.value === '_ALL_') {
-				selection = this.filter.options.map(function(o) { o.selected = e.target.checked; return o.selected && o.id;}).filter(Boolean);
+				// In this "map" function we set each option state to the "ALL" checkbox state, and return either the "id" of a selected
+				// option OR "false", then we filter all the "false" boolean out so the final result is a list of selected ids.
+				selection = this.filter.options.map(function(o) {
+					o.selected = e.target.checked;
+					return o.selected && o.id;
+				}).filter(Boolean);
+
+			// A regular option checkbox
 			} else {
-				this.filter.indeterminate = this.isIndeterminate();
-				if (!this.filter.indeterminate) {
-					this.filter.selected = this.filter.options[0] && this.filter.options[0].selected;
-				} else {
+				// If not all checkboxes are the same it is necessarily mean that the "ALL" checkbox is not selected
+				if (this.isIndeterminate()) {
 					this.filter.selected = false;
+
+				// All checkboxes are the same, set the "ALL" checkbox to be like them
+				} else {
+					this.filter.selected = this.filter.options[0] && this.filter.options[0].selected;
 				}
 
 				selection = this.filter.options.map(function(o) { return o.selected && o.id;}).filter(Boolean);
 			}
+
+		// Radios
 		} else {
-			if (this.filter.selected !== '_ALL_') {
-				selection.push(this.filter.selected);
-			}
+			// Need to update the selected option properties according to the radio selection (which is saved on this.filter.selected)
+			var self = this;
+			selection = this.filter.options.map(function(o) {
+				o.selected = self.filter.selected === o.id;
+				return o.selected && o.id;
+			}).filter(Boolean);
 		}
 
 		if (typeof this.onChange === 'function') {
--- filters/filter-list-component.js
+++ filters/filter-list-component.js
@@ -13,11 +13,15 @@
 		this.dirtyFilters = false;
 	}
 
+	/**
+	 * Raised by angular when bindings has changed
+	 * @param {object.<string, object>} changes - The changes object
+	 */
 	FilterListController.prototype.$onChanges = function(changes) {
-		// Check if the data has changed
+		// Check if the 'filters' binding has changed
 		if (typeof changes.filters !== 'undefined' && this.filters) {
+			// Clone the filters array, we don't want to hold reference to the original object
 			var tmp = [];
-
 			for (var i = 0; i < this.filters.length; ++i) {
 				var currFilter = this.filters[i];
 				var f = new FilterModel(currFilter.id, currFilter.text, !!currFilter.multi, null, currFilter.selectionState);
@@ -32,17 +36,38 @@
 		}
 	};
 
+	/**
+	 * Raised whenever a filter selection has changed
+	 * @param {string} id - The filter id
+	 * @param {[]} selection - Array of selected valued
+	 */
 	FilterListController.prototype.onFilterChange = function(id, selection) {
-		console.log(id, selection);
 		this.dirtyFilters = true;
 	};
 
-	FilterListController.prototype.applyFilters = function(id, selection) {
-		if (typeof this.onChange === 'function') {
-			this.onChange();
+	/**
+	 * Handler for the "apply" filters button
+	 */
+	FilterListController.prototype.applyFilters = function() {
+		// Once apply button has been clicked we can hide it
+		this.dirtyFilters = false;
+
+		// If no subscribers to our change event we can stop
+		if (typeof this.onChange !== 'function') {
+			return;
 		}
 
-		this.dirtyFilters = false;
+		// Create a list of active filters and raise event
+		var filters = [];
+		for (var i = 0; i < this.filters.length; ++i) {
+			var currFilter = this.filters[i];
+			var selection = currFilter.options.map(function(o) {return o.selected && o.id;}).filter(Boolean);
+			if (selection.length > 0) {
+				filters.push({id: currFilter.id, selection: selection});
+			}
+		}
+
+		this.onChange({filters: filters});
 	};
 
 	angular.module('viewMgrApp').component('pxboFilterList', {
